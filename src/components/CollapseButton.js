42b851c7c8540bfa4c97ea5b0fd4c5794c86d91b
Sergey Mikhaylovskih <for.mikhe@gmail.com>
2019-03-05 12:36:39 +0000

    changed html symbol

--- src/components/CollapseButton.js
+++ src/components/CollapseButton.js
@@ -8,7 +8,7 @@ export default class CollapseButton extends React.PureComponent {
                 onClick={() => {
                     this.props.onClick();
                 }}>
-                    &#17;
+                    &#9666;
             </button>
     }
 }
